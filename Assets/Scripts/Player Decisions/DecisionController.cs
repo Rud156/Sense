c4083f6ff027c93823910be35b5b4db4ad5ef2a4
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-10-05 23:07:42 -0600

    Testing Decision Making

---
+++ Assets/Scripts/Player Decisions/DecisionController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PlayerDecisions
+{
+    public class DecisionController : MonoBehaviour
+    {
+        [Header("Control Display")] public Image leftArrowImage;
+        public Image rightArrowImage;
+        public Image topArrowImage;
+        public Image bottomArrowImage;
+
+
+
+        #region Singleton
+
+        private static DecisionController _instance;
+
+        public static DecisionController Instance => _instance;
+
+        private void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this;
+            }
+
+            if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Player Decisions/DecisionPoint.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Player;
+using UnityEngine;
+using Utils;
+
+namespace PlayerDecisions
+{
+    public class DecisionPoint : MonoBehaviour
+    {
+        [Header("Nearby Decision Points")]
+        public DecisionPoint leftPoint;
+        public DecisionPoint rightPoint;
+        public DecisionPoint topPoint;
+        public DecisionPoint bottomPoint;
+
+        [Header("Positions")] public Transform decisionPointPosition;
+
+        private bool _isPlayerInside;
+
+        #region Unity Functions
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(TagManager.Player))
+            {
+                _isPlayerInside = true;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag(TagManager.Player))
+            {
+                _isPlayerInside = false;
+            }
+        }
+
+        private void Update()
+        {
+            if (!_isPlayerInside)
+            {
+                return;
+            }
+        }
+
+        #endregion
+
+        #region External Functions
+
+        public Vector3 GetDecisionPointPosition() => decisionPointPosition.position;
+
+        #endregion
+    }
+}
